7a6ed15152bc67d96b896c442247c7ac0a657196
kimhong <try.kimhong@institute.pse.ngo>
2025-12-10 09:41:38 +0700

    update some code and files

---
+++ src/main/java/Group2/MyMemory/service/ServiceImpl/MemoryServiceImpl.java
@@ -0,0 +1,25 @@
+// package Group2.MyMemory.service.ServiceImpl;
+
+// import Group2.MyMemory.service.MemoryService;
+// import org.springframework.stereotype.Service;
+// import org.springframework.transaction.annotation.Transactional;
+// import lombok.RequiredArgsConstructor;
+// import Group2.MyMemory.repository.MemoryRepository;
+// import Group2.MyMemory.repository.UserRepository;
+// import Group2.MyMemory.repository.CategoryRepository;
+
+
+
+// @Service
+// @Transactional
+// @RequiredArgsConstructor
+// public class MemoryServiceImpl implements MemoryService{
+//     private final MemoryRepository memoryRepository;
+//     private final UserRepository userRepository;
+//     private final CategoryRepository categoryRepository;
+
+
+
+
+//     // Implementation of the methods defined in MemoryService interface
+// }
